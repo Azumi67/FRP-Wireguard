ebc558775195d78da67084677f1022995cb4587f
Azumi <119934376+Azumi67@users.noreply.github.com>
2023-10-18 02:36:12 -0700

    Update Wire.sh
    added mutli config for wireguard

--- Wire.sh
+++ Wire.sh
@@ -78,17 +78,13 @@ function display_logo() {
 
 # art
 logo=$(cat << "EOF"
-.----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .-----------------.
-| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
-| |     ____     | || |   ______     | || |     _____    | || |  _______     | || |      __      | || | ____  _____  | |
-| |   .'    `.   | || |  |_   __ \   | || |    |_   _|   | || | |_   __ \    | || |     /  \     | || ||_   \|_   _| | |
-| |  /  .--.  \  | || |    | |__) |  | || |      | |     | || |   | |__) |   | || |    / /\ \    | || |  |   \ | |   | |
-| |  | |    | |  | || |    |  ___/   | || |      | |     | || |   |  __ /    | || |   / ____ \   | || |  | |\ \| |   | |
-| |  \  `--'  /  | || |   _| |_      | || |     _| |_    | || |  _| |  \ \_  | || | _/ /    \ \_ | || | _| |_\   |_  | |
-| |   `.____.'   | || |  |_____|     | || |    |_____|   | || | |____| |___| | || ||____|  |____|| || ||_____|\____| | |
-| |              | || |              | || |              | || |              | || |              | || |              | |
-| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
- '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' 
+    ______    _______   __      _______        __      _____  ___  
+   /    " \  |   __ "\ |" \    /"      \      /""\    (\"   \|"  \ 
+  // ____  \ (. |__) :)||  |  |:        |    /    \   |.\\   \    |
+ /  /    ) :)|:  ____/ |:  |  |_____/   )   /' /\  \  |: \.   \\  |
+(: (____/ // (|  /     |.  |   //      /   //  __'  \ |.  \    \. |
+ \        / /|__/ \    /\  |\ |:  __   \  /   /  \\  \|    \    \ |
+  \"_____/ (_______)  (__\_|_)|__|  \___)(___/    \___)\___|\____\)
 EOF
 )
 
@@ -274,6 +270,161 @@ WantedBy=multi-user.target" | sudo tee /etc/systemd/system/azumifrps.service &>/
 
   display_checkmark $'\e[92mFRP Wireguard tunnel setup has been completed successfully!\e[0m'
 }
+function multi_port() {
+clear
+    echo +═══════════════════════════+
+    echo -e "\e[93mFRP Mutli Menu\e[0m"
+    echo +════════════════════════════+
+    echo "Select an option:"
+    echo -e "1. \e[92mKharej Tunnel\e[0m"
+    echo -e "2. \e[96mIran Tunnel\e[0m"
+    echo -e "3. \e[33mBack to main menu\e[0m"
+    read -p "Enter your choice Please: " choice
+
+    case $choice in
+        1)
+            kharej_tunnel_menu
+            ;;
+        2)
+            iran_tunnel_menu
+            ;;
+        3)
+            clear
+            main_menu
+            ;;
+        *)
+            echo "Invalid choice."
+            ;;
+ esac
+}
+function kharej_tunnel_menu() {
+    clear
+     echo +═══════════════════════════+
+    echo -e "\e[93mKharej Multi Menu\e[0m"
+    echo +════════════════════════════+
+    read -p $'\e[93mNumber of Kharej IPv6 addresses needed: \e[0m' num_ipv6
+    sleep 1
+    echo "Generating Config for you..."
+
+    read -p $'\e[93mEnter \e[92mIran\e[93m IPv6 address: \e[0m' iran_ipv6
+    read -p $'\e[93mEnter Tunnel \e[92mToken\e[93m:[Same value for Kharej & Iran] \e[0m' frp_token
+    read -p $'\e[93mEnter \e[92mTunnel\e[93m Port:[Example: 443] \e[0m' tunnel_port
+
+
+    cat > frp_0.51.3_linux_amd64/frpc.ini <<EOL
+[common]
+server_addr = $iran_ipv6
+server_port = $tunnel_port
+authentication_mode = token
+token = $frp_token
+
+EOL
+
+    for ((i=1; i<=$num_ipv6; i++)); do
+        read -p $'\e[93mEnter your \e[92mKharej\e[93m '$i$'th IPv6 \e[93maddress:\e[0m\e[93m[Enter your Kharej IPV6s]\e[0m ' kharej_ipv6
+        read -p $'\e[93mEnter \e[92mKharej\e[93m port for Wireguard:\e[0m\e[92m[This is your current Wireguard port]\e[0m ' kharej_port
+        read -p $'\e[93mEnter \e[92mIran\e[93m port for Wireguard:\e[0m\e[92m[This will be your Wireguard port]\e[0m ' iran_port
+ 
+    
+        cat >> frp_0.51.3_linux_amd64/frpc.ini <<EOL
+
+[wireguard$i]
+type = udp
+local_port = $kharej_port
+remote_port = $iran_port
+local_ip$i = $kharej_ipv6
+use_encryption = true
+use_compression = true
+
+EOL
+    done
+
+    display_checkmark $'\e[92mKharej configuration generated. Yours Truly, Azumi.\e[0m'
+# Add the service section for Kharej
+    cat > /etc/systemd/system/azumifrpc.service <<EOL
+[Unit]
+Description=frpc service
+After=network.target
+
+[Service]
+ExecStart=/root/frp_0.51.3_linux_amd64/./frpc -c /root/frp_0.51.3_linux_amd64/frpc.ini
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOL
+echo "Reloading daemon..." > /dev/null 2>&1
+    systemctl daemon-reload > /dev/null 2>&1
+
+    echo "Enabling FRP service..." > /dev/null 2>&1
+    systemctl enable azumifrpc > /dev/null 2>&1
+
+    echo "Starting FRP  service..."
+    systemctl start azumifrpc
+    display_checkmark $'\e[92mFRP Service started.\e[0m'
+}
+function iran_tunnel_menu() {
+    echo +═════════════════════════════════+
+    echo -e "\e[93mIRAN Multi Menu\e[0m"
+    echo +═════════════════════════════════+
+    read -p $'\e[93mNumber of Iran IPv6 addresses needed: \e[0m' num_ipv6
+    sleep 1
+    echo "Generating Iran Config for you..."
+    read -p $'\e[93mEnter \e[92mTunnel Port\e[93m:[Example: \e[92m443\e[93m] \e[0m' tunnel_port
+    read -p $'\e[93mEnter \e[92mTunnel Token\e[93m:[\e[93mSame Value for both Kharej & Iran\e[93m] \e[0m' token
+    
+    echo -e "\e[93mGenerating config for you...\e[0m"
+    cat > frp_0.51.3_linux_amd64/frps.ini <<EOL
+[common]
+bind_port = $tunnel_port
+token = $token
+
+EOL
+
+for ((i=1; i<=$num_ipv6; i++)); do
+        read -p $'\e[93mEnter your \e[92mIran\e[93m/Server\e[92m '$i$'th IPv6 \e[93maddress:\e[0m\e[93m[Enter your Iran IPV6s]\e[0m ' iran_ipv6
+        read -p $'\e[93mEnter \e[92mKharej\e[93m port for Wireguard:\e[0m\e[92m[This is your current Wireguard port]\e[0m ' kharej_wireguard_port
+        read -p $'\e[93mEnter \e[92mIran\e[93m port for Wireguard:\e[0m\e[92m[This will be your new Wireguard port]\e[0m ' iran_wireguard_port
+ 
+    
+        cat >> frp_0.51.3_linux_amd64/frps.ini <<EOL
+[wireguard$i]
+type = tcp
+local_ip$i = $iran_ipv6
+local_port = $iran_wireguard_port
+remote_port = $kharej_wireguard_port
+use_encryption = true
+use_compression = true
+
+EOL
+    done
+
+    display_checkmark $'\e[92mIran configuration generated. Yours Truly, Azumi.\e[0m'
+# Add the service section for Kharej
+    cat > /etc/systemd/system/azumifrps.service <<EOL
+[Unit]
+Description=frps service
+After=network.target
+
+[Service]
+ExecStart=/root/frp_0.51.3_linux_amd64/./frps -c /root/frp_0.51.3_linux_amd64/frps.ini
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOL
+echo "Reloading daemon..." > /dev/null 2>&1
+    systemctl daemon-reload > /dev/null 2>&1
+
+    echo "Enabling FRP service..." > /dev/null 2>&1
+    systemctl enable azumifrps > /dev/null 2>&1
+
+    echo "Starting FRP  service..."
+    systemctl start azumifrps
+    display_checkmark $'\e[92mFRP Service started.\e[0m'
+}
 # uninstal function
 function uninstall() {
   display_notification $'\e[93mStarting uninstallation of FRP service...\e[0m'
@@ -340,14 +491,16 @@ function main_menu() {
   width=60
   padding=$((($width - ${#text}) / 2))
   printf "%*s\e[92m%s\e[0m%*s\n" $padding '' "$text" $padding ''
-  echo
-  echo
-  echo -e "\e[93mFRP Menu:\e[0m"
+  printf "\e[93m+ꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥꕥ+\e[0m\n" 
+  echo -e "\e[93m╔═════════════════════════════════════════════╗\e[0m"  
+  echo -e "\e[93m║                  Main Menu                  ║\e[0m"   
+  echo -e "\e[93m╠═════════════════════════════════════════════╣\e[0m" 
   echo -e "\e[37m1. \e[96mInstall FRP"
   echo -e "\e[37m2. \e[32mFRP Wireguard tunnel setup"
-  echo -e "\e[37m3.\e[1m \e[38;5;33mUninstall FRP Service\e[0m"
-  echo -e "\e[37m4. \e[33;1mRestart Service\e[0m"
-  echo -e "\e[37m5. \e[91;1mExit\e[0m"
+  echo -e "\e[37m3. \e[35mFRP Multi Wireguard tunnel setup"
+  echo -e "\e[37m4.\e[1m \e[38;5;33mUninstall FRP Service\e[0m"
+  echo -e "\e[37m5. \e[33;1mRestart Service\e[0m"
+  echo -e "\e[37m0. \e[91;1mExit\e[0m"
 
 read -p $'\e[5mEnter your choice Please: \e[0m' choice in
 
@@ -358,13 +511,16 @@ read -p $'\e[5mEnter your choice Please: \e[0m' choice in
     2)
       configure_frp
       ;;
-    3)
+	3)
+      multi_port
+      ;;	  
+    4)
       uninstall
       ;;
-    4)
+    6)
       restart_service
       ;;
-    5)
+    0)
       exit 0
       ;;
     *)
@@ -395,5 +551,3 @@ function restart_service() {
 }
 
 main_menu
-
-
